917d91db63559173214f9bf28488e1611efbd41d
chrloch <christian.loch@gmail.com>
2022-01-06 14:41:55 +0100

    upload download revocation

--- specs/gateway/Revocation/UpDownDelete.md
+++ specs/gateway/Revocation/UpDownDelete.md
@@ -6,14 +6,22 @@ tags: DGC_GW, Revocation
 
 Test cases for the upload and download of revocation lists 
 
-## Country A uploads, country B downloads
+| timeframe | 
+|-----------|
+|         0 |
+|         1 |
+|         7 | 
 
-Country A creates a revocation list and encrypts it for country B. 
-Country B proceeds to download the list 
+## Download revocation list from <timeframe> days ago
+
+* download revocatin list from <timeframe> days ago
+
+## First country uploads, second country downloads
 
 * create a revocation list of type "UVCI" with "500" entries
-* add revocation list recipient "DE"
-* add revocation list recipient "BE"
-* encrypt and sign CMS as "DX"
-* upload CMS as "DX"
-* download CSM as "DE"
+* sign revocation list as first country
+* upload revocation list as first country
+* download revocation list from second country
+
+___
+* delete all uploaded revocation lists
--- step_impl/gateway/revocation.py
+++ step_impl/gateway/revocation.py
@@ -18,10 +18,13 @@ from base64 import b64encode
 
 import requests
 import json
+import uuid
 
 from hashlib import sha256
 from random import randbytes
 
+from os import path
+from datetime import datetime, timedelta
 from asn1crypto import cms
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
@@ -29,32 +32,92 @@ from cryptography.x509.oid import NameOID
 from getgauge.python import data_store, step
 from requests import Response
 from step_impl.util import baseurl, certificateFolder
+from step_impl.util.certificates import create_cms
+
+
+REVOCATION_LIST_PATH = '/revocation-list'
 
 @step("create a revocation list of type <hashtype> with <num_entries> entries")
 def create_a_revocation_list_of_type_with_entries(hashtype, num_entries):
     revocation_list = [b64encode(sha256(randbytes(64)).digest()).decode('utf-8') for n in range(int(num_entries))]
-    data_store.scenario["revocation.list"] = json.dumps(revocation_list) 
+    data_store.scenario["revocation.list"] = json.dumps(revocation_list)  
+    #add_temp_file(name='crl.json', contents=json.dumps(revocation_list))
+    #add_temp_file(name='encrypted.cms', contents='')
+    #data_store.scenario["revocation.commandline"] = 'openssl cms -in crl.json -encrypt -out encrypted.cms'.split()
 
 
-@step("add revocation list recipient <country>")
-def add_revocation_list_recipient(country):
-    if not "revocation.recipient_certs" in data_store.scenario:
-        data_store.scenario["revocation.recipient_list"] = []
+@step("sign revocation list as first country")
+def sign_revocation_list_as_first_country():
+    upload_cert = x509.load_pem_x509_certificate(
+        open(path.join(certificateFolder, "upload.pem"), "rb").read())
+    upload_key = serialization.load_pem_private_key(
+        open(path.join(certificateFolder, "key_upload.pem"), "rb").read(), None)
+    return sign_revocation_list( upload_cert, upload_key )
 
-    for upload_cert in data_store.scenario['trustlist.UPLOAD']:
-        if upload_cert['country'].upper() == country.upper():
-            decorated_pem = '-----BEGIN CERTIFICATE-----\n'+upload_cert['rawData']+'\n-----END CERTIFICATE-----\n'
-            x509.load_pem_x509_certificate(bytes(decorated_pem,'utf-8'))
-            print(upload_cert['kid'])
+def sign_revocation_list( upload_cert, upload_key ):
+    'Creates a CMS message from the revocation list using the upload cert'
+    revocation_list = data_store.scenario["revocation.list"]
 
-@step("encrypt and sign CMS as <country>")
-def encrypt_and_sign_cms_as(country):
+    data_store.scenario["revocation.list.signed"] = create_cms(
+        bytes(revocation_list, 'utf-8'), upload_cert, upload_key)
+
+    return data_store.scenario["revocation.list.signed"]
+
+@step("download revocatin list from <days> days ago")
+def download_revocatin_list_from_days_ago(days):
+    pivot_date = datetime.now() - timedelta(days=int(days))
+    date_str = pivot_date.isoformat(timespec='hours') + ':00:00Z'
+    return get_revocation_list(if_modified_since=date_str)
+
+def get_revocation_list(if_modified_since='2021-06-01T00:00:00Z'):
+    response = requests.get(f"{baseurl}{REVOCATION_LIST_PATH}", 
+        headers={'If-Modified-Since': if_modified_since},
+        cert=(path.join(certificateFolder, "auth.pem"), path.join(certificateFolder, "key_auth.pem")))
+    data_store.scenario["response"] = response
+    print("Response: ", response.status_code,  response.text)
+    assert response.ok
+    data_store.scenario["revocation-list.batches"] = response.json()['batches']
+
+def get_revocation_list_batch(batchId):
+    response = requests.get(f"{baseurl}{REVOCATION_LIST_PATH}/{batchId}", 
+        cert=(path.join(certificateFolder, "auth.pem"), path.join(certificateFolder, "key_auth.pem")))
+    data_store.scenario["response"] = response
+
+
+@step("upload revocation list as first country")
+def upload_revocation_list_as_first_country():
+    cert=(path.join(certificateFolder, "auth.pem"), path.join(certificateFolder, "key_auth.pem"))
+    return upload_revocation_list(cert)
+
+def upload_revocation_list(cert):
+    assert "revocation.list.signed" in data_store.scenario
+    if not "rev.lists.created" in data_store.spec:
+        data_store.spec["rev.lists.created"] = []
+
+    batch_id = str(uuid.uuid4())
+    headers = {"Content-Type": "application/cms",
+               "Content-Transfer-Encoding": "base64",
+               "ETag" : batch_id,
+               }
+    response = requests.post(f"{baseurl}{REVOCATION_LIST_PATH}", data=data_store.scenario["revocation.list.signed"], headers=headers, cert=cert)
+    data_store.scenario["response"] = response
+    print("Response: ", response.status_code,  response.text)
+    # for cleanup later
+    if response.ok:
+        data_store.spec["rev.lists.created"].append( {
+            'used_tls_cert' : cert, 
+            'batch_id' : batch_id
+        } )
+
+
+
+@step("download revocation list from second country")
+def download_revocation_list_from_second_country():
     assert False, "Add implementation code"
 
-@step("upload CMS as <DX>")
-def upload_cms_as(DX):
-    assert False, "Add implementation code"
 
-@step("download CSM as <DE>")
-def download_csm_as(DE):
-    assert False, "Add implementation code"
+@step("delete all uploaded revocation lists")
+def delete_all_uploaded_revocation_lists():
+    if "rev.lists.created" in data_store.spec:
+        for uploaded_batch in data_store.spec["rev.lists.created"]:
+            assert False, "Add code"
